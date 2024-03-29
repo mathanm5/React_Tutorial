ec6f1a3f6cd3a6ff39da328c5a29a1dd2efb7164
Mathanprasath K <mathancitra7@gmail.com>
2024-03-29 21:24:53 +0530

    Add files via upload
    Here what we are going to see means "Lists and Keys" suppose, we have a list of items to be shown on the webpage, what we have to do means create an array of objects and call it in JSX format of map( ) function and arrow function ( )=> by creating an <ul> or <ol> and keys are like each element we can give any one of the variable will have a unique name right , So keeping that variable as a key, otherwise list contains only numerics means we can set index value as a key.

--- Garage.js
+++ Garage.js
@@ -6,12 +6,23 @@ function Garage(){
     //const carInfo ={}
     const carInfo = {brand:"Ferrari", color:"Red"}
     const appleInfo = {type:"Fuji", color:"Green"}
+    const carList = [{brand:"BMW", color:"Green"},
+                    {brand:"Audi", color:"yellow"},
+                    {brand:"Mercedes", color:"White"},];
+
+    const NumList =[1,2,3,3,4,5,6,7,7,8,9,10];
     const showcar = carInfo.brand !== undefined && carInfo.color !== undefined
     return(
         <>
         {/*<Car brand={brand} color={'Black'}>*/}
         {showcar?<Car carInfo={carInfo}/>:null}
         <Apple appleInfo={appleInfo}/>
+        <ul>
+            {carList.map((carInfo)=> <li key={carInfo.brand}><Car carInfo={carInfo}/></li>)}
+        </ul>
+        <ul>
+            {NumList.map((e,index)=><li key={index}>{e}</li>)}
+        </ul>
         </>
     );
 }
