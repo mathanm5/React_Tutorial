81c50135dbc26eb898ab3bf77df0c38cdad1f103
Mathanprasath K <mathancitra7@gmail.com>
2024-03-29 20:44:38 +0530

    Add files via upload
    Here what we are going to see means CONDITIONAL RENDERING, consider a situation like, you do not have aproper props of a particular statement visible on webpage , at that time, it will print "Undefined", if a props become undefined we want to hide that, so here comes the conditional rendering by using(ternary operator and AND operator), here i have used in car.js file i have checked the conditional rendering.Simply if proper props available it will show this on webpage otherwise it will not show on the webpage.

--- Garage.js
+++ Garage.js
@@ -3,12 +3,14 @@ import Apple from "./Apple";
 
 function Garage(){
     //const brand = 'Bentely';
+    //const carInfo ={}
     const carInfo = {brand:"Ferrari", color:"Red"}
     const appleInfo = {type:"Fuji", color:"Green"}
+    const showcar = carInfo.brand !== undefined && carInfo.color !== undefined
     return(
         <>
         {/*<Car brand={brand} color={'Black'}>*/}
-        <Car carInfo={carInfo}/>
+        {showcar?<Car carInfo={carInfo}/>:null}
         <Apple appleInfo={appleInfo}/>
         </>
     );
